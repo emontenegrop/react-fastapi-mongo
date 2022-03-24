9c04270b946ae9b6e1743d058386ae794ee6f74d
Edwin Montenegro <edwin.montenegro@sercop.gob.ec>
2022-03-24 16:21:32 -0500

    Aprendiendo React

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+let func = (a) => {
+  return " Hola Mundo" 
+}
+
+const element = <h1>Hello, work!</h1>
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> aqui estamos {func(12)}   {element}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
