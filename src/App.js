f66b5ddba89713f4c8f3e8c8185d34cfcaeb48cf
Edwin Montenegro <montenegro.edwin@gmail.com>
2022-03-28 16:08:34 -0500

    Consumo de servicio Rest

--- src/App.js
+++ src/App.js
@@ -1,72 +1,14 @@
-
+import React from 'react';
 import './App.css';
-import { useState } from 'react'
-import PageWrapper from './components/PageWrapper';
-import Paginacion from './components/Paginacion';
-import Pelicula from './components/Pelicula';
-
+import ListadoPeliculas from './ListadoPeliculas';
 
 
 function App() {
 
-  const [paginaActual, setPaginaActual] = useState(1);
-  const [peliculas, setPeliculas] = useState([]);
-
-  const TOTAL_POR_PAGINA = 4;
-
-
-  const buscarPeliculas = async () => {
-    let url = 'http://localhost/prueba/Peliculas.json';
-    //Funcion asincronica
-    let respuesta = await fetch(url, {
-      'method': 'GET',
-      'mode':'no-cors',
-      'headers': {
-        'Accept': 'application/json',        
-        'Content-Type': 'application/json; charset=utf-8'
-      }
-    });
-
-    let peliculasJson = await respuesta.json();
-    setPeliculas(peliculasJson);
-  };
-
-
-  const cargarPelicualas = () => {
-    /* peliculas = peliculas.slice(
-       (paginaActual - 1) * TOTAL_POR_PAGINA,
-       (paginaActual * TOTAL_POR_PAGINA)
-     );*/
-  }
-
-  const getTotalPaginas = () => {
-    let cantidadTotalPeliculas = peliculas.length;
-    return Math.ceil(cantidadTotalPeliculas / TOTAL_POR_PAGINA)
-  }
-
-
-
   return (
-    <PageWrapper>
-      <button onClick={buscarPeliculas}>Prueba</button>
-
-      {peliculas.map(pelicula =>
-        <Pelicula titulo={pelicula.titulo} calificacion={pelicula.calificacion}
-          director={pelicula.director} actores={pelicula.actores}
-          fecha={pelicula.fecha} duracion={pelicula.duracion} imagen={pelicula.imagen}>
-          {pelicula.descripcion}
-        </Pelicula>
-      )}
-
-      <Paginacion pagina={paginaActual} total={getTotalPaginas()} onChange={(pagina) => {
-        setPaginaActual(pagina)
-      }}>
-
-      </Paginacion>
-
-    </PageWrapper >
-
-
+    <div>
+     <ListadoPeliculas/>
+    </div>
   );
 }
 
--- src/components/Pelicula.js
+++ src/components/Pelicula.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function Pelicula(props) {
   return (
     <div className="movie-item-style-2">
